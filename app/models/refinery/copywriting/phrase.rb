12d422c0d92675cf0a49deaa51d08fcf238c05f9
Enrique <icodeat9>
2021-10-30 10:04:10 -0400

    deprecated method

--- app/models/refinery/copywriting/phrase.rb
+++ app/models/refinery/copywriting/phrase.rb
@@ -6,10 +6,8 @@ module Refinery
       translates :value if self.respond_to?(:translates)
       validates :name, :presence => true
 
-      attr_accessible :locale, :name, :default, :value, :scope, :page, :page_id, :phrase_type
-
       if self.respond_to?(:translation_class)
-        self.translation_class.send :attr_accessible, :locale
+        self.translation_class.send :attr_accessor, :locale
       end
 
       default_scope order([:scope, :name])
