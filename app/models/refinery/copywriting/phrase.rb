113a78540eb80fa979e56c64c1411a7bdeced88b
Robert Marcelino <robert.marcelino@outlook.com>
2025-05-12 18:12:16 -0400

    fix issues of  pages

--- app/models/refinery/copywriting/phrase.rb
+++ app/models/refinery/copywriting/phrase.rb
@@ -2,29 +2,47 @@ module Refinery
   module Copywriting
     class Phrase < Refinery::Core::BaseModel
 
-      belongs_to :page, :class_name => 'Refinery::Page'
-      validates :name, :presence => true
+      belongs_to :page, class_name: 'Refinery::Page', optional: true
+      translates :value if respond_to?(:translates)
+      validates :name, presence: true
 
+      # Default scope for ordering phrases
+      default_scope { order(:scope, :name) }
 
-      default_scope { order([:scope, :name]) }
-
+      # Find or create a phrase by name and options
       def self.for(name, options = {})
-        options = {:phrase_type => 'text', :scope => 'default'}.merge(options.reject {|k,v| v.blank? })
+        options = default_options.merge(options.compact_blank)
         options[:name] = name.to_s
-        options[:page_id] ||= options[:page].try(:id)
+        options[:page_id] ||= options[:page]&.id
 
-        phrase = self.find_by(name: options[:name], scope: options[:scope], page_id: options[:page_id]) || self.create(options)
-        phrase.update(options.except(:value, :page, :page_id, :locale))
-        phrase.last_access_at = Date.today
-        phrase.save if phrase.changed?
-
+        phrase = find_or_create_phrase(options)
+        phrase.update_phrase_attributes(options)
         phrase.default_or_value
       end
 
+      # Return the value if present, otherwise return the default
       def default_or_value
-        value.present? ? value : default
+        value.presence || default
       end
 
+      private
+
+      # Default options for a phrase
+      def self.default_options
+        { phrase_type: 'text', scope: 'default' }
+      end
+
+      # Find or create a phrase by name, scope, and page_id
+      def self.find_or_create_phrase(options)
+        find_by(name: options[:name], scope: options[:scope]) || create(options)
+      end
+
+      # Update phrase attributes and save if changed
+      def update_phrase_attributes(options)
+        assign_attributes(options.except(:value, :page, :page_id, :locale))
+        self.last_access_at = Date.today
+        save if changed?
+      end
     end
   end
 end
