b81854386af8ec65c827c8d33e5c31f5c4d9b789
Robert Marcelino <robert.marcelino@outlook.com>
2025-05-12 18:44:30 -0400

    fix issues of  pages

--- app/controllers/refinery/copywriting/admin/phrases_controller.rb
+++ app/controllers/refinery/copywriting/admin/phrases_controller.rb
@@ -2,46 +2,49 @@ module Refinery
   module Copywriting
     module Admin
       class PhrasesController < ::Refinery::AdminController
-        before_action :find_scope
-        before_action :find_all_scopes
+        before_action :find_all_locales, :find_locale, :find_scope, :find_all_scopes
 
-        crudify :'refinery/copywriting/phrase', 
-                searchable: false,
-                title_attribute: 'name',
-                sortable: false,
-                redirect_to_url: 'refinery.copywriting_admin_phrases_path',
-                include: [:translations]
+        crudify :'refinery/copywriting/phrase', :searchable => false,
+                :title_attribute => 'name', :xhr_paging => true, :sortable => false,
+                :redirect_to_url => 'refinery.copywriting_admin_phrases_path'
 
-        protected
+      protected
 
-        # Fetch all phrases, optionally filtered by scope
         def find_all_phrases
-          @phrases = Phrase.where(page_id: nil)
-          @phrases = @phrases.where(scope: @scope) if @scope.present?
+          @phrases = Phrase.where(:page_id => nil)
+
+          if find_scope
+            @phrases = @phrases.where(:scope => find_scope)
+          end
         end
 
-        # Find the current scope from params
+        def find_locale
+          @current_locale ||= (params[:switch_locale].try(:to_sym) || Thread.current[:globalize_locale] || default_locale).to_sym
+        end
+
+        def find_all_locales
+          @locales ||= ::Refinery::I18n.frontend_locales
+        end
+
         def find_scope
-          @scope = params[:filter_scope].presence
+          @scope ||= params[:filter_scope]
         end
 
-        # Fetch all unique scopes for phrases without a page
         def find_all_scopes
-          @scopes = Phrase.where(page_id: nil).distinct.pluck(:scope)
+          @scopes ||= Phrase.select(:scope).where(:page_id => nil).map(&:scope).uniq
         end
 
-        # Strong parameters for phrase
-        def phrase_params
-          params.require(:phrase).permit(
-            :name, 
-            :default, 
-            :value, 
-            :scope, 
-            :page, 
-            :page_id, 
-            :phrase_type
-          )
+        def default_locale
+          ::Refinery::I18n.default_frontend_locale
         end
+
+        def globalize!
+          super
+          if params[:switch_locale]
+            Thread.current[:globalize_locale] = (params[:switch_locale] || default_locale).try(:to_sym)
+          end
+        end
+
       end
     end
   end
